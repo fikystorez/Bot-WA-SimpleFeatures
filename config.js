91be697a8cf475aee0839fb84392cbf9fa02b781
Fiky Store <masfikystorez@gmail.com>
2026-05-09 09:04:47 +0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    channelUrl: "https://whatsapp.com/channel/0029VbBZ9AmJ3jupObG7To0m",
-    channelName: "📢 Saluran Official",
-    channelJid: "120363422427738408@newsletter" 
-};
